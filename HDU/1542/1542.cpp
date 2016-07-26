61d33b19147e72a6ea356fd78d27e8366583efe0
New-bottle <775712558@qq.com>
2016-07-26 22:04:23 +0800

    add HDU

---
+++ HDU/1542/1542.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+const int N = 110;
+struct node{
+	double x1, y1, x2, y2;
+}a[N];
+
+int n;
+
+int main(){
+
+	return 0;
+}
