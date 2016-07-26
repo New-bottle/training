c14719990282687fa769db625af69bc432736b37
New-bottle <775712558@qq.com>
2016-07-26 21:16:20 +0800

    A Accepted / learn java from code of team Gungnir

--- 160723/A/Main.java
+++ 160723/A/Main.java
@@ -1,60 +1,54 @@
-import java.util.*;
+//learn java from the code of team Gungnir
 import java.math.*;
+import java.util.*;
 
 public class Main{
-	static int NNN = 1000000;
-	static BigInteger N, M;
+	static int LEN = 1_000_000;
+	static BigInteger n, m;
 	static BigInteger Zero = new BigInteger("0");
 	static BigInteger One = new BigInteger("1");
 	static BigInteger Two = new BigInteger("2");
-	static BigInteger[] queue = new BigInteger[NNN];
-	static BigInteger[] num_step = new BigInteger[NNN];
+	static BigInteger[] queue = new BigInteger[LEN];
+	static BigInteger[] num_step = new BigInteger[LEN];
 	static Collection c = new HashSet();
 	public static void main(String[] args){
 		Scanner cin = new Scanner(System.in);
 		while(true){
-			N = cin.nextBigInteger();
-			M = cin.nextBigInteger();
-			BigInteger ans = N.add(M);
-			if (N.compareTo(Zero) == 0 && M.compareTo(Zero) == 0) break;
+			n = cin.nextBigInteger();
+			m = cin.nextBigInteger();
+			if (n.compareTo(Zero) == 0 && m.compareTo(Zero) == 0) break;
+			BigInteger ans = (n.subtract(m)).abs();
 			c.clear();
-			c.add(N);
-			int head = 1;
-			int tail = 1;
-			queue[1] = N;
+			c.add(n);
+			int head = 1, tail = 1;
+			queue[1] = n;
 			num_step[1] = Zero;
-			BigInteger tmp, p;
+			BigInteger x, tmp, now, p;
 			while(head <= tail){
-				N = queue[head]; 
-				BigInteger now = num_step[head ++];
-				tmp = ((N.subtract(M)).abs()).add(now);
+				x = queue[head];
+				now = num_step[head ++];
+				tmp = (x.subtract(m).abs()).add(now);
+				if (ans.compareTo(tmp) > 0) ans = tmp;
 				now = now.add(One);
-				if (tmp.compareTo(ans) < 0) ans = tmp;
-				if (N.compareTo(Zero) != 0){
-					if ((N.mod(Two)).compareTo(Zero) == 0){
-						p = N.divide(Two);
-						if (c.contains(p) == false){
-							c.add(p);
-							tail ++;
-							queue[tail] = p;
-							num_step[tail] = now;
-						}
-					}else{
-						p = N.add(One);
-						if (c.contains(p) == false){
-							c.add(p);
-							tail ++;
-							queue[tail] = p;
-							num_step[tail] = now;
-						}
-
-						p = N.subtract(One);
-						if (c.contains(p) == false){
-							c.add(p);
-							tail ++;
-							queue[tail] = p;
-							num_step[tail] = now;
-						}
+				if ((x.mod(Two)).compareTo(Zero) == 0){
+					p = x.divide(Two);
+					if (c.contains(p) == false){
+						c.add(p);
+						queue[++ tail] = p;
+						num_step[tail] = now;
+					}
+				}else{
+					p = x.add(One);
+					if (c.contains(p) == false){
+						c.add(p);
+						queue[++ tail] = p;
+						num_step[tail] = now;
+					}
+					p = x.subtract(One);
+					if (c.contains(p) == false){
+						c.add(p);
+						queue[++ tail] = p;
+						num_step[tail] = now;
 					}
 				}
 			}
@@ -62,3 +56,5 @@ public class Main{
 		}
 	}
 }
+
+
