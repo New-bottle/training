ad9bf69b9fec46d189f22640de4f842c4577a619
New-bottle <775712558@qq.com>
2017-03-28 08:49:13 +0800

    C Accepted

--- 2017spring/0311/C.cpp
+++ 2017spring/0311/C.cpp
@@ -6,8 +6,9 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 100010;
-int to[N << 1], nxt[N << 1], head[N], cnt;
+const int N = 200010;
+int to[N << 2], nxt[N << 2], head[N], cur[N], cnt = 1;
+bool del[N << 2];
 void add(int x, int y)
 {
 	to[++cnt] = y; nxt[cnt] = head[x]; head[x] = cnt;
@@ -15,6 +16,7 @@ void add(int x, int y)
 }
 
 int n, m, num[N], odd[N], du[N], belong[N], tot;
+int st[N], top, ans, route[N << 2];
 vector<int>have[N];
 bool vis[N];
 
@@ -27,16 +29,40 @@ void dfs(int x)
 			dfs(to[i]);
 }
 
-int ans, route[N << 1];
 void getans(int x)
 {
-	route[++ans] = x;
+//	printf("getans %d\n", x);
 	vis[x] = 1;
-	for (int i = head[x]; i; i = nxt[i])
-		if (!vis[to[i]]) {
+	st[++top] = x;
+	for (int &i = cur[x]; i; i = nxt[i])
+		if (!del[i]) {
+			del[i] = del[i ^ 1] = 1;
 			getans(to[i]);
-			route[++ans] = x;
+			break;
 		}
+}
+
+void fleury(int x)
+{
+//	printf("fluery %d\n", x);
+	vis[x] = 1;
+//	top = 1;
+	st[++top] = x;
+	while (top)
+	{
+		int k = 0;
+		for (int i = cur[st[top]]; i; i = nxt[i])
+			if (!del[i]) {
+				k = 1; break;
+			}
+		if (!k) {
+			route[++ans] = st[top--];
+//			printf("%d ", st[top]); --top;
+		} else {
+			--top;
+			getans(st[top + 1]);
+		}
+	}
 }
 int main()
 {
@@ -60,14 +86,18 @@ int main()
 			have[belong[i]].push_back(i);
 		}
 	for (int i = 1; i <= tot; ++i)
-		for (int j = 1; j < (int)have[i].size() - 1; j += 2)
+		for (int j = 1; j < (int)have[i].size() - 1; j += 2) {
 			add(have[i][j - 1], have[i][j]);
+			++du[have[i][j - 1]]; ++du[have[i][j]];
+		}
 
+	for (int i = 1; i <= n; ++i) cur[i] = head[i];
 	memset(vis, 0, sizeof vis);
 	for (int i = 1; i <= n; ++i)
-		if (!vis[i] && (du[i] & 1)) getans(i);
+		if ((du[i] & 1) && !vis[i]) fleury(i);
+	for (int i = 1; i <= n; ++i)
+		if (!vis[i] && du[i]) fleury(i);
 	printf("%d\n", ans);
-	for (int i = 1; i <= ans; ++i)
-		printf("%d%c", route[i], i == ans ? '\n' : ' ' );
+	for (int i = 1; i <= ans; ++i) printf("%d%c", route[i], i == ans ? '\n' : ' ');
 	return 0;
 }
